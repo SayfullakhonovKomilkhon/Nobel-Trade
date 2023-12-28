deebd5bdb7944f53a2cc35c6896290ebacdcc113
sayfik2021 <sayfik2021@mail.ru>
2023-12-28 14:27:00 +0500

    Our advantages page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Advantages from "./components/Advantages";
 
 
 function App() {
   return (
     <div>
       <Router>
-        <Header/>
+        <Advantages/>
         <Routes>
 
         </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import '../sass/header.scss';
-import logo from '../images/logo.svg'
 import capsule from '../images/header-capsule-text.svg'
-import headerLogoOil from '../images/header-logo.svg'
 import aboutImg from "../images/header-capsule-text-mini.svg";
 import rectengleImg from "../images/squre-elemrnt.svg";
 import videoPlayer from "../images/videoplayer.png";
@@ -12,14 +10,12 @@ import top5Card from '../images/top-col-5-card.png'
 import top3CardMain from '../images/top-col-3-card.png'
 import bottom3CardMini from '../images/bottom-col-3-card.png'
 import arrow from '../images/arrow-top-right-large.svg'
-import footerLogo from  '../images/footer-logo.svg'
-import instagram from '../images/white-instagram.svg'
-import telegram from '../images/white-telegram.svg'
-import phone from '../images/phone icon.svg'
-import location from '../images/location icon.svg'
 import years15 from '../images/15years-truck picture.png'
 import uzbekistanMap from '../images/Uzbekistan map.png'
 import oil8000 from '../images/8000-oil.png'
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Application from "./Application";
 
 
 const Header = () => {
@@ -27,27 +23,7 @@ const Header = () => {
         <>
             <div className="home">
                 <header className='header'>
-                    <nav className="navbar navbar-expand-lg navbar-light">
-                        <a className="navbar-brand" href="#"><img className="headerImg" src={logo} alt=""/></a>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                                aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
-                                <a className="nav-item nav-link active" href="#">ГЛАВНАЯ <span
-                                    className="sr-only">(current)</span></a>
-                                <a className="nav-item nav-link" href="#">О КОМПАНИИ</a>
-                                <a className="nav-item nav-link" href="#">НАШИ ПРЕИМУЩЕСТВА</a>
-                                <a className="nav-item nav-link " href="#">КОНТАКТЫ</a>
-                            </div>
-                            <div className="changeLanguage">
-                                <div className="rus">rus</div>
-                                <div className="uzb">uzb</div>
-                            </div>
-                        </div>
-                    </nav>
+                    <Navbar/>
                     <section className='header-info'>
                         <div className="row">
                             <div className="col-7">
@@ -177,60 +153,11 @@ const Header = () => {
                                 </div>
 
                             </div>
-                            <div className="be-partners">
-                                <p className="subtitle">
-                                    CТАНОВИТЕСЬ ПАРТНЕРАМИ И<br/> РАЗВИВАЙТЕСЬ ВМЕСТЕ С
-                                </p>
-                                <button className='btn'>ОТПРАВИТЬ ЗАЯВКУ <img src={arrow} alt=""/></button>
-                            </div>
-                            <p className='main-text'>NOBEL TRADE</p>
+                            <Application/>
                         </div>
                     </section>
                 </div>
-                <footer className='footer'>
-                    <div className="footer-components">
-                        <div className="footer-form">
-                            <p>АКТУАЛЬНЫЕ ВАКАНСИИ В<br/> НАШЕЙ КОМПАНИИ</p>
-                            <button className='btn'>ОТПРАВИТЬ<br/>  СВОИ ДАННЫЕ <img src={arrow} alt=""/></button>
-                        </div>
-                        <div className="footer-navigate">
-                            <div className="row">
-                                <div className="col-lg-4 main">
-                                    <img src={footerLogo} alt=""/>
-                                    <ul className='navbar'>
-                                        <li className='nav-item nav-link'>ГЛАВНАЯ</li>
-                                        <li className='nav-item nav-link'>О КОМПАНИИ</li>
-                                        <li className='nav-item nav-link'>ПАРТНЕРЫ</li>
-                                        <li className='nav-item nav-link'>НАШИ ПРЕИМУЩЕСТВА</li>
-                                    </ul>
-                                </div>
-                                <div className="col-lg-2 main-part">
-                                    <ul className='navbar'>
-                                        <li className='nav-item nav-link'>КАТАЛОГ</li>
-                                        <li className='nav-item nav-link'>ВАКАНСИИ</li>
-                                        <li className='nav-item nav-link'>КОНТАКТЫ</li>
-                                    </ul>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="footer-socials">
-                                        <img src={instagram} alt=""/>
-                                        <img src={telegram} alt=""/>
-                                    </div>
-                                    <div className="footer-contacts">
-                                        <p className="phone-number">
-                                            <img src={phone} alt=""/>
-                                            +998 71 209 33 35
-                                        </p>
-                                        <p className="address">
-                                            <img src={location} alt=""/>
-                                            г. Ташкент, Кичик Халка Йули 4
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+                <Footer/>
             </div>
         </>
     );
